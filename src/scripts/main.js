6cb613f5972ede2fa2b4cc6e04b5ff93153e39dc
kira134679 <kira134679@gmail.com>
2025-11-03 22:54:15 +0800

    feat: implement render product list from api response

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
+import axios from 'axios';
 import '../styles/main.css';
+import { formatedPrice } from './utils/helpers';
+
+const productList = document.querySelector('.productWrap');
+
+async function init() {
+  await getProductList();
+}
+
+await init();
+
+async function getProductList() {
+  try {
+    const res = await axios.get(
+      `${import.meta.env.VITE_API_BASE_URL}/api/livejs/v1/customer/${import.meta.env.VITE_API_PATH}/products`,
+    );
+    renderProductList(res.data.products);
+  } catch (error) {
+    console.log(error);
+    alert(error.response);
+    return;
+  }
+}
+
+function renderProductList(products) {
+  let contents = '';
+  products.forEach(p => {
+    contents += `<li class="productCard">
+                   <h4 class="productType">新品</h4>
+                   <img
+                     src="${p.images}"
+                     alt=""
+                   />
+                   <a href="#" class="addCardBtn">加入購物車</a>
+                   <h3>${p.title}</h3>
+                   <del class="originPrice">NT${formatedPrice(p.origin_price)}</del>
+                   <p class="nowPrice">NT${formatedPrice(p.price)}</p>
+                 </li>`;
+  });
+
+  productList.innerHTML = contents;
+}
