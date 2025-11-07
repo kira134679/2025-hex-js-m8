01b98520eb14b232ad57929ca5f575503acad6bf
kira134679 <kira134679@gmail.com>
2025-11-07 22:17:13 +0800

    feat: add timestamp conversion utility function

--- src/scripts/utils/helpers.js
+++ src/scripts/utils/helpers.js
@@ -1,3 +1,9 @@
 export function formatedPrice(price) {
   return new Intl.NumberFormat('zh-TW', { style: 'currency', currency: 'TWD', maximumFractionDigits: 0 }).format(price);
 }
+
+export function timestampToDate(timestamp) {
+  const datetime = new Date(timestamp * 1000);
+  const datetimeArr = [datetime.getFullYear(), datetime.getMonth() + 1, datetime.getDate()];
+  return datetimeArr.join('/');
+}
